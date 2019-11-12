843a1b2d149d01051dcfe3500d48efd04887cb71
alexr <belloa@hartwick.edu>
2019-11-11 23:17:31 -0500

    Created Functions and the code

--- Car_Behaviors.py
+++ Car_Behaviors.py
@@ -1,1 +1,22 @@
+from Car_Class import Car
 
+make = "Pontiac"
+
+model = "Sunfire"
+
+color = "Blue"
+
+year = "2006"
+
+mileage = 50000
+
+price = "1,500"
+
+#Car.description()
+
+
+my_car = Car(make, model, color, year, mileage, price)
+my_car.description()
+my_car.change_color("Green")
+my_car.odometer_update(1002)
+my_car.description()
--- Car_Class.py
+++ Car_Class.py
@@ -1,1 +1,23 @@
+# Alex Bello
+# 11/11/2019
+# Defines the Class Car
 
+
+class Car():
+    def __init__(self, make, model, color, year, mileage, price):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+        self.mileage = mileage
+        self.price = price
+
+    def description(self):
+        print(f"""The cars make is {self.make} the model is {self.model} it's a {self.color} {self.year} {self.model}. 
+It has {self.mileage} miles on it and I only got it for {self.price} dollars!""")
+
+    def change_color(self,new_color):
+        self.color = new_color
+
+    def odometer_update(self,more_miles):
+        self.mileage += more_miles
